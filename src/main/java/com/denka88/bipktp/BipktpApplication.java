12ba1a448b6427e9cb701c5c8360fe2cd4bb03b8
Denka88 <animedenisik.com@mail.ru>
2025-06-11 14:46:59 +0300

    init

---
+++ src/main/java/com/denka88/bipktp/BipktpApplication.java
@@ -0,0 +1,13 @@
+package com.denka88.bipktp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BipktpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BipktpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/denka88/bipktp/BipktpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denka88.bipktp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BipktpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
