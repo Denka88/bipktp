ed69d63392be316a66d083f774f5a186bf1e9d93
Denka88 <animedenisik.com@mail.ru>
2025-06-11 21:08:47 +0300

    RecordRepo fix

--- src/main/java/com/denka88/bipktp/repo/RecordRepo.java
+++ src/main/java/com/denka88/bipktp/repo/RecordRepo.java
@@ -1,5 +1,6 @@
 package com.denka88.bipktp.repo;
 
+import com.denka88.bipktp.model.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecordRepo extends JpaRepository<Record, Long> {
